346a9aca821200b8e0bd4d0658e46e8b41378558
Rishit Gupta <rishitgupta@Rishits-MacBook-Air.local>
2024-02-06 15:06:09 -0800

    Updated Social Page

--- src/Contact.js
+++ src/Contact.js
@@ -6,6 +6,19 @@ function ContactPage() {
       <h1>Contact Page</h1>
       <p>This is the Contact Page of our application.</p>
       <p>It provides information about our socials and how to reach out to us.</p>
+      
+      {/* Instagram link */}
+      <a href="https://www.instagram.com/" target="_blank">
+        Follow us on Instagram
+      </a>
+      <br />
+      <br      />
+      <a href="https://nextdoor.com/" target="_blank">
+        Check our updates on nextdoor
+      </a>
+      <br  />
+      <br  />
+      <a href="https://www.gofundme.com/f/schools-in-need-of-computers" target="_blank" rel="noopener noreferrer">GoFundme to Donate</a>
     </div>
   );
 }
