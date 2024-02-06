865bb1f4fabda9a0322d7c62a2a93e51f832084a
Rishit Gupta <rishitgupta@Rishits-MacBook-Air.local>
2024-02-05 23:06:51 -0800

    footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar'; // Correct path to Navbar component
-import Home from './Home'; // Correct path to Home component
+import Navbar from './Navbar';
+import Home from './Home';
+import Footer from './Footer'; // Import the Footer component
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
+        <Footer /> {/* Include the Footer component */}
       </div>
     </Router>
   );
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; // Assuming you'll style your footer
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Copyright © Techcore 2024. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
