1e08e0e48fffd42797093332ea6de030cc8cbca1
Rishit Gupta <rishitgupta@Rishits-MacBook-Air.local>
2024-02-07 15:12:58 -0800

    update page

--- src/updates.js
+++ src/updates.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import pic01 from './pic01.jpg';
-import './updates.css'; // Import CSS file for styling
 
 function Updates() {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${pic01})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh', // Set minimum height to fill the viewport
+    padding: '30px',
+  };
+
+  const horizontalLineStyle = {
+    width: '100%', // Set the width of the horizontal line to 100%
+    margin: '20px 0', // Adjust margin for spacing
+    borderTop: '2px solid black', // Increase the thickness and style of the line
+  };
+
   return (
-    <div>
-        
+    <div style={backgroundImageStyle}>
+      <div style={{ padding: '20px' }}>
+        {/* Add your content here */}
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />    
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <hr style={horizontalLineStyle} /> {/* Horizontal line */}
     </div>
   );
 }
