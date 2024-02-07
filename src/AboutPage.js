416861a8b55106db2dfab47d63326bae8c67acdd
Rishit Gupta <rishitgupta@Rishits-MacBook-Air.local>
2024-02-06 19:52:49 -0800

    New updates ig

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import websiteImage from './website.jpg'; // Import the image
+import pic01 from './pic01.jpg'; 
 
 function AboutPage() {
   return (
     <div style={{
-      backgroundImage: `url(${websiteImage})`,
+      backgroundImage: `url(${pic01})`,
       backgroundSize: 'cover',
       height: '100vh',
-      color: 'white' // Set text color to white
+      color: 'white', 
+      padding: '20px', 
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
     }}>
-      <h1>About Page</h1>
-      <p>This is the About page of our application.</p>
-      <p>It provides information about our application.</p>
+      <div style={{
+        backgroundColor: 'black', 
+        padding: '100px', 
+        border: '4px blue', 
+        borderRadius: '10px', 
+        textAlign: 'center', 
+      }}>
+        <h1>TechCore - Providing Access to Technology for Schools</h1>
+        <p>
+          Donating Computers to Schools
+          <br /> 
+          TechCore is a company that strives to provide access to technology for schools by donating used and new computers.
+          <br /> 
+          We believe that every student should have the opportunity to learn and grow with the help of modern technology. 
+          <br />
+          With the help of our donors, we are able to provide schools with the resources they need to succeed.
+        </p>
+      </div>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,27 @@
-// HomePage.js
 import React from 'react';
 
 function HomePage() {
   return (
-    <div style={{ backgroundColor: '#9899A1', height: '100vh' }}>
-      <h1>Our Mission:</h1>
+    <div style={{ backgroundColor: '#9899A1', height: '100vh', padding: '20px' }}>
+      <br />
+      <br />
+      <h1>Our Mission: Provide Affordable Computers to Schools</h1>
       <p>TechCore is on a mission to bridge the digital divide by providing affordable computers to schools. <br />
         We believe that every student deserves access to technology, and we're working hard to make that a reality.</p>
+      <div style={{ backgroundColor: 'white', padding: '10  px', border: '2px solid black', borderRadius: '20px', width: '20%', margin: '2 0px auto' }}>
+        <p style={{ fontWeight: 'bold' }}>Our Solution:</p>
+        <p>
+          We understand that every school can't afford computers for every student. <br />
+          That's why we want to donate used laptops to schools so every student has access to technology in the classroom.
+        </p>
+      </div>
+      <br />
+      <div style={{ backgroundColor: 'white', padding: '10  px', border: '2px solid black', borderRadius: '20px', width: '20%', margin: '2 0px auto' }}>
+        <p style={{ fontWeight: 'bold' }}>TechCore in the Community :</p>
+        <p>
+        We're proud to be a part of the community and give back in any way we can. Hosting Computer Donation Drives, raising money to purchase computers.
+        </p>
+      </div>
     </div>
   );
 }
