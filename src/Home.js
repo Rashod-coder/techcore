f08a325c0dab702a12a87066791e580c25f3ea61
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-04-20 14:52:14 -0700

    home page updated

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { FaLaptop, FaHandsHelping, FaDesktop, FaUserFriends } from 'react-icons/fa';
+import { Container, Row, Col } from 'reactstrap'; // Import Container, Row, Col from Reactstrap
 import AOS from 'aos'; 
 import 'aos/dist/aos.css'; 
 import { Card, CardHeader, CardBody, CardTitle, CardText, Button, CardFooter } from 'reactstrap'; // Import Reactstrap components
 import './output.css';
 import { Analytics } from '@vercel/analytics/react';
-import Tblue from './cesar-couto-sKuVjm0xyLY-unsplash.jpg'; // Import the background image
-import laptop from './Color logo with background.png';
+import Tblue from './blues.jpg'; // Import the background image
+import laptop from './Color logo - no background.png';
+import './home.css'
 
 function HomePage() {
   useEffect(() => {
@@ -14,20 +16,20 @@ function HomePage() {
   }, []);
 
   return (
-    <div className="homepage" style={{ backgroundImage: `url(${Tblue})`, backgroundSize: 'cover', backgroundPosition: 'center', width: '100vw', height: '100vh', marginTop: '-150px' }}>
-      <div style={{marginTop: '0px'}}>
+    <div>
+      <div className="homepage" style={{ backgroundImage: `url(${Tblue})`, backgroundSize: 'cover', backgroundPosition: 'center', width: '100%', height: '150vh', marginTop: '-150px' }}>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <main className="py-6 px-4 sm:p-9 md:py-10 md:px-8">
-          <div className="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
-            <div className="flex flex-col justify-center lg:col-start-1 lg:row-start-1 lg:row-span-6">
-              <h1 className="col-span-6 text-xl md:text-2xl lg:text-3xl font-semibold mb-4 lg:mb-6 text-center">Striving to reduce the Digital Divide in education</h1>
-              <Analytics />
-            </div>
-            <div className="grid gap-8 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
-              <img src={laptop} alt="Techcore" style = {{width: '400px', height: '400px'}}className="w-300 h-300 object-cover rounded-lg lg:col-span-full" loading="lazy" />
-            </div>
-          </div>
-        </main>
+        <Container>
+          <Row>
+            <Col lg="9" md = "12 " sm="12" className="mb-4 mb-md-0">  
+              <br/><br/><br/><br/>
+              <h1 style={{fontSize: '50px'}}className='text-center'>Striving to Reduce the Digital Divide in Education</h1>
+            </Col>     
+            <Col lg="3" md="12" sm="12" className="text-center">
+            <img src={laptop} alt="Laptop" className="logo-img" />
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
