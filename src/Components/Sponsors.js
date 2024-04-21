8c9b1513df23a8b1e3dce192a2fd91ae04b8626d
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-04-21 13:47:05 -0700

    background

--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -8,7 +8,7 @@ function TextExample() {
           <div className="card mx-auto" style={{ backgroundColor: '#D7E3F4' }}>
             <div className="card-body">
               <h3 className="card-title">Sponsors</h3>
-              <p style={{fontSize: '24px'}}className="card-text">Techcore is currently looking for sponsors to help support our cause, if you would like to sponsor us please email us at <a href="mailto:example@example.com">project.techcore@gmail.com</a> to get started. </p>      
+              <p style={{fontSize: '24px'}}className="card-text">Techcore is currently looking for sponsors to help support our cause, if you would like to sponsor us please email us at <a href="mailto:project.techcore@gmail.com">project.techcore@gmail.com</a> to get started. </p>      
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import 'aos/dist/aos.css';
 import './output.css';
 import { Analytics } from '@vercel/analytics/react';
 import background from './Assets/smooth.jpg'; // Import the background image
+import background1 from './Assets/pool.jpg'; // Import the background image
 import logo from './Assets/Tech.png';
 import './home.css'
 import Cards from './Components/Cards'
@@ -18,17 +19,17 @@ function HomePage() {
 
   return (
     <div>
-      <div className="homepage" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center', width: '100%', height: '130vh', marginTop: '-160px' }}>
+      <div className="homepage" style={{ backgroundImage: `url(${background1})`, backgroundSize: 'cover', backgroundPosition: 'center', width: '100%', height: '130vh', marginTop: '-160px' }}>
       <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
         <Container>
           <Row>
-            <Col lg="9" md = "12 " sm="12" className="mb-4 mb-md-0">  
+            <Col lg="9" md = "12 " sm="12" className="mb-4 mb-md-0 mb-5 mt-5">  
               <br/><br/><br/><br/>
-              <h1 style={{fontSize: '50px'}}className='text-center'>Striving to Reduce the Digital Divide in Education</h1>
+              <h1 style={{fontSize: '50px', color: 'black'}}className='text-center'>Striving to Reduce the Digital Divide in Education</h1>
             </Col>     
-            <Col lg="3" md="12" sm="12" className="text-center">
-            <img src={logo} alt="Laptop" className="logo-img" />
+            <Col lg="3" md="12" sm="12" className="text-center mb-5 mt-5">
+            <img src={logo} alt="Laptop" className="logo-img " />
             </Col>
           </Row>
         </Container>
