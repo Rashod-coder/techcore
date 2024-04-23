62b4e6c7727fc787dde1b7d6db90ba156fb67645
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-04-23 14:48:35 -0700

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; Techcore is fiscally sponsored by The Hack Foundation (d.b.a. Hack Club), a 501(c)(3) nonprofit (EIN: 81-2908499)</p>
+      <p>&copy; Techcore 2024 <br/> Techcore is fiscally sponsored by The Hack Foundation (d.b.a. Hack Club), a 501(c)(3) nonprofit (EIN: 81-2908499)</p>
 
     </footer>
   );
