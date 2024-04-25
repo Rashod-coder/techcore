2293b4acc5a1f61caf60ab46713fa8fa2e9b167b
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-04-24 19:51:11 -0700

    Update Sign.js

--- src/Components/Sign.js
+++ src/Components/Sign.js
@@ -11,6 +11,7 @@ function TextExample() {
               <p style={{fontSize: '24px', fontFamily: 'oxygen', color: 'white'}}className="card-text"> If you are currently in grades 5-8 please fill out this interest form so we can get a idea on what classes to teach & after that we will be sending out registration link to register in about 2-3 weeks </p>
               <p style={{fontSize: '24px', fontFamily: 'oxygen', color: 'white'}}className="card-text"> If you are currently in Highschool and would like to earn volunteer hours by teaching these classes 1 hour class twice per week please sign up here. </p>
     </div>
+  </div>
   );
 }
 
